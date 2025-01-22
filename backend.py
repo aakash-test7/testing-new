97da1c2cb0d49fbad51588c5349a1f787dea51cf
aakash-test7 <akgdsc7@gmail.com>
2025-01-22 14:28:05 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -359,8 +359,7 @@ def multi_transcriptid_info(mtid):
                 else:
                     st.write(f"No match found for Transcript id: {tid} in miRNA data\n")
             if not result.empty:
-                sorted_result = result.sort_values(by="Transcript id")
-                st.dataframe(sorted_result)
+                st.dataframe(result)
 
             st.subheader("Protein and PPI data")
             protein_matching_rows = protein_df[protein_df['Transcript id'].isin(mtid_list)]
